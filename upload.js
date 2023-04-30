c716336ce60cb3c98db57f38f3dd77a190ee8d7a
Jacob Johnson <jacobjeremiah42@gmail.com>
2023-04-29 23:33:03 -0400

    sdf

--- upload.js
+++ upload.js
@@ -1,5 +1,17 @@
-const input = document.querySelector("input")
+const fileInput = document.getElementById("input")
 const output = document.getElementById("input-wrapper")
 let imagesArray = []
 var base64String;
 
+input.onchange = () => {
+    const selectedFile = fileInput.files[0];
+  console.log(selectedFile);
+
+  var reader = new FileReader();
+  reader.onloadend = function() {
+    console.log(reader.result)
+    base64String = reader.result
+  }
+  reader.readAsDataURL(selectedFile)
+  console.log(reader)
+}
