c14dc70d7338920faaf6d1487757240de56d5ab3
Jacob Johnson <jacobjeremiah42@gmail.com>
2023-04-29 23:02:48 -0400

    sd

--- upload.js
+++ upload.js
@@ -1,20 +1,5 @@
 const input = document.querySelector("input")
-const output = document.querySelector("output")
+const output = document.getElementById("input-wrapper")
 let imagesArray = []
+var base64String;
 
-input.addEventListener("change", function() {
-    const file = input.files
-    imagesArray.push(file[0])
-    display()
-})
-
-function display() {
-    let images = ""
-    imagesArray.forEach((image, index) => {
-        images += `<div class="image">
-        <img src="${URL.createObjectURL(image)}" alt="image">
-        <span onclick="deleteImage(${index})">&times;</span>
-      </div>`
-    })
-    output.innerHTML = images
-}
