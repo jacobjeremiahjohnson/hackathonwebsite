0d455e81c7244ede005b9bf74064060d47568f7c
Jacob Johnson <jacobjeremiah42@gmail.com>
2023-04-30 11:25:54 -0400

    final front end commit

--- play.js
+++ play.js
@@ -1,20 +1,96 @@
 let map;
 const markerArray = []
-const coords = { 
+var coords = { 
     lat: 39.68141109865876, 
     lng: -75.75283527374268
 }
+var lat;
+var lng;
+var description;
+var title;
+var base64;
 let roundCount = 1;
 let totalPoints = 0;
 let roundPoints = 0;
-
+var id = 0;
 const answerMarkers = []
+var ids = [ , , ]
+var photos;
 
-document.getElementById("image").src = "1.png";
+getPhotos();
+
+function checkIn(j){
+    for (let i = 0; i < 3; i++){
+        if(ids[i] == j){
+            return true;
+        }
+    }
+    return false;
+}
+
+function populateIds() {
+    for (let i = 0; i < 3; i++){
+        while(true){
+            let j = Math.floor(Math.random() * photos)
+            if(checkIn(j)){
+                continue;
+            }
+            ids[i] = j
+            break;
+        }
+    }
+    console.log(ids)
+}
+
+function getPhotos() {
+    var Http = new XMLHttpRequest();
+    var url='http://localhost:8080/'
+
+    fetch(url)
+    .then(data=>{return data.json()})
+    .then(res=>{
+        photos = res;
+        populateIds();
+        getInfo()
+    })
+}
+
+
+function getInfo() {
+    let id = ids[roundCount-1].toString()
+    var Http = new XMLHttpRequest();
+    var url='http://localhost:8080/GET/'+id+'/json';
+
+    fetch(url)
+    .then(data=>{return data.json()})
+    .then(res=>{
+        console.log(res)
+        lat = parseFloat(res.latitude);
+        lng = parseFloat(res.longitude);
+        description = res.description;
+        title = res.name;
+        console.log(lat + "" + lng)
+
+        coords.lat = lat;
+        coords.lng = lng;
+    })
+
+    const xhr = new XMLHttpRequest();
+    xhr.open("GET", "http://localhost:8080/GET/"+id+"/image", true);
+    xhr.responseType = "blob";
+    xhr.onload = function() {
+    let img = document.getElementById("image")
+    img.src = URL.createObjectURL(xhr.response);
+    };
+    xhr.send();
+}
+
+
 
 document.getElementById("next-round-button").addEventListener("click", function() {
     document.getElementById("round-count").innerHTML = "Round " + roundCount + "/3"
     document.getElementById("next-round-button").style.visibility = "hidden"
+    document.getElementById("submit-button").style.visibility = "visible"
     markerArray[0].setMap(null)
     markerArray[0] = null
     markerArray.shift()
@@ -25,17 +101,21 @@ document.getElementById("next-round-button").addEventListener("click", function(
     answerMarkers[0] = null
 
     answerMarkers.shift()
+    document.getElementById("description-title").innerHTML = "";
+    document.getElementById("description-description").innerHTML = "";
+
+    id = id + 1;
+    getInfo()
 })
 
 function endScreen() {
-    totalPoints = totalPoints + roundPoints
 
     let finalscore = document.getElementById("final-score")
     finalscore.style.visibility = "visible"
-    finalscore.innerHTML = "Final Score: " + totalPoints.toFixed(2)
+    finalscore.innerHTML = "Final Score: " + totalPoints.toFixed(2) + " / 30.00"
 
-    document.getElementById("points").innerHTML = "Points: " + totalPoints.toFixed(2)
-
+    document.getElementById("points").innerHTML = "Points: " + totalPoints.toFixed(2) + " + " + roundPoints.toFixed(2);
+    document.getElementById("submit-button").style.visibility = "hidden";
 }
 
 function nextRound(){
@@ -48,6 +128,7 @@ function nextRound(){
         document.getElementById("points").innerHTML = "Points: " + totalPoints.toFixed(2) + " + " + roundPoints.toFixed(2);
         totalPoints = totalPoints + roundPoints
         roundPoints = 0
+        document.getElementById("submit-button").style.visibility = "hidden";
     }
 }
 
@@ -61,8 +142,10 @@ function calcDistance(x1, x2, y1, y2){
 function calcPoints(distance){
     if(distance > 15){
         roundPoints = 0;
+        return;
     } else if (distance < 1) {
         roundPoints = 10;
+        return;
     }
     roundPoints = 10 - (distance * 0.66666666666)
 }
@@ -78,13 +161,21 @@ document.getElementById("submit-button").addEventListener("click", function() {
 
     calcDistance(coords.lat, guessedLat, coords.lng, guessedLng)
 
+    
+
     var answer = new google.maps.Marker({
         position: coords,
         map: map,
         icon: "beachflag.png"
     })
 
+    map.setCenter(answer.getPosition());
+
     answerMarkers.push(answer)
+
+
+    document.getElementById("description-title").innerHTML = title;
+    document.getElementById("description-description").innerHTML = description;
 
     nextRound()
 });
--- upload.js
+++ upload.js
@@ -1,17 +1,82 @@
-const fileInput = document.getElementById("input")
+var fileInput = document.getElementById("input")
 const output = document.getElementById("input-wrapper")
+var title = document.getElementById("text-title")
+var description = document.getElementById("text-input")
+var image = document.getElementById("image");
+var latitude = document.getElementById("latitude")
+var longitude = document.getElementById("longitude")
 let imagesArray = []
-var base64String;
+var file;
+
+document.getElementById("auto-location").onclick = function() {
+  
+  navigator.geolocation.getCurrentPosition(success, error);
+
+  function success(pos) {
+    const crd = pos.coords;
+
+    latitude.value = crd.latitude;
+    longitude.value = crd.longitude;
+  }
+
+  function error() {
+    document.getElementById("auto-location").textContent = "Location not supported"
+  }
+
+}
 
 input.onchange = () => {
-    const selectedFile = fileInput.files[0];
-  console.log(selectedFile);
+    file = fileInput.files[0];
+  console.log(file);
+  image.src = URL.createObjectURL(file);
 
-  var reader = new FileReader();
-  reader.onloadend = function() {
-    console.log(reader.result)
-    base64String = reader.result
+  const url = image.src;
+
+  document.getElementById("upload-photo").style.visibility = "visible"
+  
+}
+
+document.getElementById("upload-photo").onclick = function() {
+
+  if (title.value != null && description.value != null && latitude.value != null && longitude.value != null){
+  var nhr = new XMLHttpRequest();
+  nhr.open('POST', 'http://localhost:8080/POST/', true);
+  nhr.setRequestHeader('Content-Type', 'image/jpeg');
+
+  nhr.onload = function() {
+    if (nhr.status === 200) {
+      console.log(nhr.responseText);
+    } else {
+      console.log('Request failed. Returned status of ' + nhr.status);
+    }
+  };
+
+  var data = file;
+  nhr.send(data);
+  
+  
+    var xhr = new XMLHttpRequest();
+
+  xhr.open('POST', 'http://localhost:8080/POST/', true);
+  xhr.setRequestHeader('Content-Type', 'application/json');
+
+  xhr.onload = function() {
+    if (xhr.status === 200) {
+      console.log(xhr.responseText);
+    } else {
+      console.log('Request failed. Returned status of ' + xhr.status);
+    }
+  };
+
+  var data = `{ "name": "` + title.value +  `", "latitude": "` + latitude.value + `", "longitude": "` + longitude.value + `", "description": "` + description.value + `" }'`;
+  console.log(JSON.stringify(data))
+  xhr.send(data);
   }
-  reader.readAsDataURL(selectedFile)
-  console.log(reader)
+
+  title = document.getElementById("text-title").value = "Image Title Here"
+  description = document.getElementById("text-input").value = "Image Description Here"
+  image = document.getElementById("image").src = ""
+  latitude = document.getElementById("latitude").value = "Latitude Here"
+  longitude = document.getElementById("longitude").value = "Longitude Here"
+
 }
